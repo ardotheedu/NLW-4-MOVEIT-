58f64817f784ff0500889eb70fa957d79d26f2c2
ardotheedu <eduardosalima3@gmail.com>
2021-03-04 15:14:50 -0300

    feat: Ranking page

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,2 +1,25 @@
+import styles from '../styles/pages/Ranking.module.css'
+
+import { SideBar } from '../components/SideBar'
+import { ToggleButton } from '../components/ToggleButton'
+import { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
+
 export default function Ranking() {
+  const {theme} = useContext(ThemeContext)
+
+  return (
+        <div className={theme === 'dark' ? `${styles.dark}` : styles.page}>
+              <SideBar />
+              <ToggleButton />
+              <div className={styles.container}> 
+                <section>
+                    <h1>Sem ranking por enquanto</h1>
+                    <p>Tente novamente mais tarde</p>
+                </section>
+              </div>
+        </div>
+  )
 }
+
+
